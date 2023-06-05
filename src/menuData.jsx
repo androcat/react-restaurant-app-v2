50a0ef6ba6e55c3b3fc94900f3ed32d8d549efd1
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 14:53:30 -0400

    Adds a quantity key

--- src/menuData.jsx
+++ src/menuData.jsx
@@ -2,25 +2,31 @@ export const menuData = [
   {
     name: "Grilled Cheese with Tomato Soup",
     price: 11,
+    qty: 0,
   },
   {
     name: "Chicken Sandwich",
     price: 12,
+    qty: 0,
   },
   {
     name: "Colombian Burger",
     price: 14,
+    qty: 0,
   },
   {
     name: "Bandeja Paisa (Country Plate)",
     price: 15,
+    qty: 0,
   },
   {
     name: "Ceviche",
     price: 15,
+    qty: 0,
   },
   {
     name: "Skirt Steak with Fries",
     price: 18,
+    qty: 0,
   },
 ];
