cf5defd34c3a89edb9e680e3856a15b6b7256c0a
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 13:53:58 -0400

    Adds some styles

--- src/MenuList.jsx
+++ src/MenuList.jsx
@@ -1,9 +1,14 @@
 import MenuItems from "./menuItems";
 
-export default function MenuList() {
+export default function MenuList({ addToOrder }) {
   return (
-    <ul style={{ width: "100%" }}>
-      <MenuItems />
-    </ul>
+    <>
+      <section>
+        <h1>Jay's Colombian American Fusion</h1>
+        <ul style={{ width: "100%" }}>
+          <MenuItems addToOrder={addToOrder} />
+        </ul>
+      </section>
+    </>
   );
 }
