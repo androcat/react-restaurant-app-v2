20ef37c711a6d883efb001c5ac7b94d216db9be0
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 13:50:20 -0400

    Displays the menu and order components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 import MenuList from "./MenuList";
+import Order from "./Order";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [orderList, setOrderList] = useState([]);
+
+  const addToOrder = function (name, price) {
+    const newOrder = { name, price };
+
+    setOrderList([...orderList, newOrder]);
+
+    console.log({ orderList });
+  };
 
   return (
     <>
-      <MenuList />
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+        }}
+      >
+        <MenuList addToOrder={addToOrder} />
+        <Order orderList={orderList} />
+      </main>
     </>
   );
 }
