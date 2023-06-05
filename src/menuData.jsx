8c3556b1a7923b4a18a92461e6accfc994b9fc4c
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 13:53:26 -0400

    Adds menu food items

--- src/menuData.jsx
+++ src/menuData.jsx
@@ -1,10 +1,26 @@
 export const menuData = [
   {
-    name: "Burger",
-    price: 13,
+    name: "Grilled Cheese with Tomato Soup",
+    price: 11,
   },
   {
     name: "Chicken Sandwich",
     price: 12,
+  },
+  {
+    name: "Colombian Burger",
+    price: 14,
+  },
+  {
+    name: "Bandeja Paisa (Country Plate)",
+    price: 15,
+  },
+  {
+    name: "Ceviche",
+    price: 15,
+  },
+  {
+    name: "Skirt Steak with Fries",
+    price: 18,
   },
 ];
