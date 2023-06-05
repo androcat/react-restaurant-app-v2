0ab192d061fb6e4d8c730bfb92acee20ef83d750
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 14:54:34 -0400

    Adds a sum total function, displays total, and adds submit button that adds order to local storage

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,6 +1,11 @@
 import "./Order.css";
 
 export default function Order({ orderList }) {
+  const sumTotal = orderList.reduce(
+    (total, currentValue) => total + currentValue.price,
+    0
+  );
+
   return (
     <aside className="order">
       <h2>Your Order</h2>
@@ -11,10 +16,23 @@ export default function Order({ orderList }) {
             style={{ display: "flex", justifyContent: "space-between" }}
           >
             <p>{orderItem.name}</p>
-            <p>{orderItem.price}</p>
+            <p>${orderItem.price}</p>
           </li>
         ))}
       </ul>
+      <p style={{ textAlign: "right" }}>Total: ${sumTotal}</p>
+      <p style={{ textAlign: "right" }}>
+        <button
+          onClick={() => {
+            localStorage.setItem(
+              "Order",
+              `Order: ${orderList.map((item) => item.name)}, Total: ${sumTotal}`
+            );
+          }}
+        >
+          Submit Order
+        </button>
+      </p>
     </aside>
   );
 }
