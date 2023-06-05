c3325a08818963e357da6b8dca6341aa3ae85f4d
Jay Medina <jay.medina.celis@gmail.com>
2023-06-05 14:55:46 -0400

    Deletes unused function

--- src/MenuItems.jsx
+++ src/MenuItems.jsx
@@ -2,15 +2,6 @@ import { menuData } from "./menuData";
 import "./MenuItems.css";
 
 export default function MenuItems({ addToOrder }) {
-  //   const [hover, setHover] = useState(false);
-  function handleClick(e) {
-    // This will prevent any synthetic events from firing after this one
-    // e.stopPropagation();
-    e.preventDefault();
-    addToOrder(item.name, item.price);
-    // e.target.style.backgroundColor = "gray";
-  }
-
   return (
     <>
       {menuData.map((item, index) => (
@@ -19,7 +10,7 @@ export default function MenuItems({ addToOrder }) {
           className="item"
           onClick={(e) => {
             e.preventDefault();
-            addToOrder(item.name, item.price);
+            addToOrder(item);
           }}
         >
           <li
